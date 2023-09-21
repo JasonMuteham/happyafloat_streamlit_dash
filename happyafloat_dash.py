8878a03f12ecfd88ba1cdde1159cb276968722c3
Jason Muteham <jason@jasonmuteham.com>
2023-09-21 10:22:27 +0100

    cumative sql

--- happyafloat_dash.py
+++ happyafloat_dash.py
@@ -5,7 +5,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 
-local = False
+local = True
 
 st.markdown('''
 
@@ -14,7 +14,7 @@ st.markdown('''
 Family Adventures at Sea.
 ''')
 st.divider()
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 @st.cache_resource(ttl=60*60)
 def define_connection(local):
@@ -34,40 +34,48 @@ fig = go.Figure(go.Indicator(
     mode = "gauge+number",
     value = nm,
     domain = {'x': [0, 1], 'y': [0, 1]},
-    title = {'text': "Nautical Miles"},
+    title = {'text': "10K Nautical Miles"},
     gauge = {'bar': {'color': "blue"},
-             'threshold' : {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': 10000}
+             'threshold' : {'line': {'color': "red", 'width': 4}, 
+                                    'thickness': 0.75, 
+                                    'value': 10000}
              }
              ))
 
-st.plotly_chart(fig)
+col1.plotly_chart(fig, use_container_width=True)
 
+@st.cache_data(ttl=60*60)
 def get_motoring_sailing_hrs():
     return con.sql("""
+                   SELECT Year, "Total Minutes", "motoring", "sailing", "Motoring %", "Sailing %", "Nautical Miles",
+                   SUM("Nautical Miles") OVER (ORDER BY Year) as "Cumulative Miles" 
+                   FROM (
                     SELECT
-                        YEAR(log_date)::varchar AS "year", 
+                        YEAR(log_date)::varchar AS "Year", 
                         SUM(motoring_minutes + (motoring_hours * 60 )) + SUM(sailing_minutes + (sailing_hours * 60 )) AS "Total Minutes",
                         SUM(motoring_minutes + (motoring_hours * 60 )) AS "motoring", 
                         SUM(sailing_minutes + (sailing_hours * 60 )) AS "sailing",
                         ((motoring / "Total Minutes")*100)::Decimal(4,1) AS "Motoring %",
-                        ((sailing / "Total Minutes")*100)::decimal(4,1) AS "Sailing %"
+                        ((sailing / "Total Minutes")*100)::decimal(4,1) AS "Sailing %",
+                        SUM(nautical_miles)::integer AS "Nautical Miles"
                     FROM raw.log_data
-                    GROUP BY year 
+                    GROUP BY Year 
+                    ORDER BY Year)
                                     
-                   """).df()
+                   """).df()  # noqa: E501
 
 motor_sail_hrs = get_motoring_sailing_hrs()
-motor_sail_hrs                   
 
-col1.line_chart(motor_sail_hrs,x="year",y=["Total Minutes","motoring","sailing"],
-                color = [[10,100,10],[.5,.5,.8], '#0000FF'])
-col2.bar_chart(motor_sail_hrs,
-               x="year",
-               y=["Motoring %","Sailing %"],
-               color = [[.5,.5,.8], '#0000FF']
-               )
+motor_sail_hrs       
 
-fig = px.bar(motor_sail_hrs, x="year", y=["Motoring %","Sailing %"],
-             barmode='group',
+
+fig = px.bar(motor_sail_hrs, x="Year", y=["Motoring %","Sailing %"],
+             barmode='group',title = "Motoring vs Sailing"
              )
-st.plotly_chart(fig)
+fig.update_layout(yaxis_title='Percent', xaxis_title='', showlegend=True, )
+fig.update_coloraxes(colorbar_ticklabelposition='inside',colorbar_ticks='inside',showscale=False)
+fig.update_traces(hovertemplate='%{y}%',marker_line_color='darkblue', marker_line_width=1.0)
+col2.plotly_chart(fig, use_container_width=True)
+
+fig = px.line(motor_sail_hrs, x="Year", y=["motoring","sailing"])
+col3.plotly_chart(fig, use_container_width=True)
